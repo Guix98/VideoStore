4215355be9d277ba808c27fdaa6763d0738e186d
Guido Hernani <guimega.gh@gmail.com>
2020-03-05 17:45:05 -0400

    routing

--- pages/movies.php
+++ pages/movies.php
@@ -4,20 +4,27 @@ $head = include('../components/header.php');
 
 <body>
     <br><br>
+    
     <h1 class="text-center">Video Store</h1>
+    
     <ul class="nav nav-pills is-danger">
+
         <li class="nav-item">
             <a class="nav-link " href="../index.php">Rents</a>
         </li>
+
         <li class="nav-item">
             <a class="nav-link" href="./customers.php">Customers</a>
         </li>
+        
         <li class="nav-item">
             <a class="nav-link active" href="./movies.php">Movies</a>
         </li>
+        
         <li class="nav-item">
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Log Out</a>
         </li>
+    
     </ul>
     <h2 class="text-center">Rent</h2>
     <div class="card text-white bg-dark" style="width: 18rem;">
